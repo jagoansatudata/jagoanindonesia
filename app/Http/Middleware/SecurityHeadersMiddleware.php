ddfb5f1e7cda567487c41764dd308ea572d3249b
ryosantoso <ryosantoso15@gmail.com>
2026-05-05 21:02:55 +0700

    fix bug error

--- app/Http/Middleware/SecurityHeadersMiddleware.php
+++ app/Http/Middleware/SecurityHeadersMiddleware.php
@@ -20,7 +20,12 @@ class SecurityHeadersMiddleware
             . "base-uri 'self'; "
             . "frame-ancestors 'self'; "
             . "form-action 'self'; "
-            . "object-src 'none';";
+            . "object-src 'none'; "
+            . "img-src 'self' data:; "
+            . "font-src 'self' data: https://fonts.gstatic.com https://fonts.bunny.net; "
+            . "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net; "
+            . "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net; "
+            . "script-src 'self' 'unsafe-inline';";
 
         $response->headers->set('Content-Security-Policy', $csp);
 
