b55ef0953894474f89503b95479d2dc6d03e3dd2
ryosantoso <ryosantoso15@gmail.com>
2026-05-05 08:06:27 +0700

    fix favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <title>{{ $title ?? 'Jagoan Indonesia' }}</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=plus-jakarta-sans:400,500,600,700,800&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/svg+xml" href="{{ asset('images/fav.svg') }}">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
